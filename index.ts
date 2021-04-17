a2dcd3ca0d314c2d89a3e7b0395c20877f345a8c
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2021-04-17 18:19:34 +0100

    logs

--- index.ts
+++ index.ts
@@ -1,31 +1,33 @@
-import * as core from "@actions/core";
+import * as core from "@actions/core"
 
 async function run() {
   try {
     if (!process.env.GITHUB_EVENT_PATH) {
-      throw new Error("GITHUB_EVENT_PATH env var not set");
+      throw new Error("GITHUB_EVENT_PATH env var not set")
     }
 
-    const isPullRequest = !!process.env.GITHUB_HEAD_REF; //GITHUB_HEAD_REF is only set for pull request events https://docs.github.com/en/actions/reference/environment-variables
+    const isPullRequest = !!process.env.GITHUB_HEAD_REF //GITHUB_HEAD_REF is only set for pull request events https://docs.github.com/en/actions/reference/environment-variables
 
-    let branchName: string;
+    let branchName: string
     if (isPullRequest && process.env.GITHUB_HEAD_REF) {
-      branchName = process.env.GITHUB_HEAD_REF;
+      branchName = process.env.GITHUB_HEAD_REF
     } else {
       if (!process.env.GITHUB_REF) {
-        throw new Error("GITHUB_EVENT_PATH env var not set");
+        throw new Error("GITHUB_EVENT_PATH env var not set")
       }
-      core.info("GITHUB_REF value: " + process.env.GITHUB_REF);
+      core.info("GITHUB_REF value: " + process.env.GITHUB_REF)
+      core.debug("GITHUB_REF value: " + process.env.GITHUB_REF)
+      console.log("GITHUB_REF value: " + process.env.GITHUB_REF)
       branchName = process.env.GITHUB_REF.split("/")
         .slice(2)
         .join("/")
-        .replace(/\//g, "-");
+        .replace(/\//g, "-")
     }
 
-    core.exportVariable("GIT_BRANCH_NAME", branchName);
+    core.exportVariable("GIT_BRANCH_NAME", branchName)
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
-run();
+run()
